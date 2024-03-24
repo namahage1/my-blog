c12b4fa2bee41c6172bc99b83adf446477e91998
Kasumi Akiyama★ <shellyakiyama@gmail.com>
2024-03-23 19:03:00 -0600

    added background color change in main page

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,1 +1,23 @@
 /*This file is used for functions to use for both index and blog.html files*/ 
+
+const themeSwitcher = document.querySelector('#theme-switcher');
+const container = document.querySelector('.container');
+const mainContainer = document.querySelector('.mainContainer');
+// Set default mode to light
+let mode = 'light';
+
+// Listen for a click event on toggle switch
+themeSwitcher.addEventListener('click', function () {
+  // If mode is dark, apply light background
+  if (mode === 'dark') {
+    mode = 'light';
+    container.setAttribute('class', 'light container');
+    mainContainer.setAttribute('class', 'light');
+  }
+  // If mode is light, apply dark background
+  else {
+    mode = 'dark';
+    container.setAttribute('class', 'dark container');
+    mainContainer.setAttribute('class', 'dark');
+  }
+});
