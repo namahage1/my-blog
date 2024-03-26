0d088a29898a89fa25ee335ce8d7eb446f6a32ec
Kasumi Akiyama★ <shellyakiyama@gmail.com>
2024-03-25 18:06:50 -0600

    localstorage in form.js is done

---
+++ README.md
@@ -0,0 +1,15 @@
+# my-blog
+
+my-blog
+├── assets
+│   ├── css
+│   │   ├── blog.css
+│   │   ├── form.css
+│   │   └── styles.css
+│   └── js
+│       ├── blog.js
+│       ├── form.js
+│       └── logic.js
+├── index.html
+├── blog.html
+└── README.md
--- assets/js/form.js
+++ assets/js/form.js
@@ -1,10 +1,34 @@
-
 //take input from form in index.html 
-document.getElementById("submitForm").addEventListener("submit",function(event){event.preventDefault();
-    let userName = document.getElementById("username").value;
-    let title = document.getElementById("title").value;
-    let content = document.getElementById("content").value;
-    console.log("user name is : " + `${userName}`
-    + "title is : " + `${title}`
-    +" content is : " + `${content}`);  
+
+const userName = document.getElementById("username");
+    const title = document.getElementById("title");
+    const content = document.getElementById("content");
+    let userInfo = null;
+
+document.getElementById("submitForm").addEventListener("submit",function(event){
+    
+    event.preventDefault();
+
+    userInfo={
+        userName: userName.value,
+        title: title.value,
+        content: content.value.trim(),
+    };
+
+    console.log("user name is : " + `${userName.value}`
+    + "title is : " + `${title.value}`
+    +" content is : " + `${content.value.trim()}`); 
+
+    localStorage.setItem('userInfo', JSON.stringify(userInfo));
+    userName.textContext  = userName;
+    display();
+    window.location.href = "./blog.html";
 });
+
+function display(){
+    const userInput = JSON.parse(localStorage.getItem('userInfo'));
+    if (userInput !== null) {
+        document.querySelector('#usenameInput').textContent =
+          userInfo.userName + ` ${userInput.title}`;
+      }
+}
